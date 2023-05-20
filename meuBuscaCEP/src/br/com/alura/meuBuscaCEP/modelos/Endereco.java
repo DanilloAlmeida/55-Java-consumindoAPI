6899515427a317b6ea2449ff26149fce4a0cb789
Danillo <danillodca@gmail.com>
2023-05-20 17:25:47 -0300

    subindo meuBuscaCep

---
+++ meuBuscaCEP/src/br/com/alura/meuBuscaCEP/modelos/Endereco.java
@@ -0,0 +1,5 @@
+package br.com.alura.meuBuscaCEP.modelos;
+
+public record Endereco(String cep, String logradouro, String  complemento, String localidade, String uf) {
+
+}
---
+++ meuBuscaCEP/src/br/com/alura/meuBuscaCEP/principal/Main.java
@@ -0,0 +1,29 @@
+package br.com.alura.meuBuscaCEP.principal;
+
+import br.com.alura.meuBuscaCEP.modelos.ConsultaCep;
+import br.com.alura.meuBuscaCEP.modelos.Endereco;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+        Endereco novoEndereco;
+
+        System.out.println("Digite o CEP: ");
+        String cepEntrada = leitura.nextLine();
+
+        try {
+//            novoEndereco = consultaCep.buscaEndereco("01");
+//            novoEndereco = consultaCep.buscaEndereco("01001000");
+            novoEndereco = consultaCep.buscaEndereco(cepEntrada);
+            System.out.println(novoEndereco);
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("\n\n-------->>>> fim normal");
+    }
+}
